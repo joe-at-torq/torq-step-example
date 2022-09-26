66cab15c8c67c684dcf9f0c8c6dab9f316ffe6aa
Joe Dillig <97898347+joe-at-torq@users.noreply.github.com>
2022-09-26 15:37:51 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,9 @@
-
-if [ -z "$KUBERNETES_SERVICE_HOST" ] #Restrict this step to only execute from inside a Docker Runner environment. (Unsupported on kubernetes)
+if [ -z "$COMMAND" ] #Check if required paramater is passed.
 then
-    if [ -z "$COMMAND" ] #Check if required paramater is passed.
-    then
-        echo "{\"error\":\"no command provided\"}"
-        exit 9
-    else    
-        echo "{\"command\":\"your command is $COMMAND\"}"
-        exit 0
-    fi
-else
-    echo "  {\"error\":\"this step is only allowed to execute from a runner.\"}"
+    echo "{\"error\":\"no command provided\"}"
     exit 9
+else    
+    echo "{\"command\":\"your command is $COMMAND\"}"
+    exit 0
 fi
 
